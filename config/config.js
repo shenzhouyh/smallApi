3d6b333e383bc96113a89eb1742d244fe6c8bc3d
shenzhouyh <286817442@qq.com>
2019-11-17 02:02:42 +0800

    第一次引用API接口，并实现页面展示

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    appKey: "NIUg9jsXK3OjEXGi",
+    apiBaseUrl: "http://se.7yue.pro/v1/"
+}
+//使用es6 方式进行导出
+export {config}
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,88 @@
 // pages/home/home.js
+import {config} from "../../config/config";
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        topTheme:null,
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        wx.request({
+            //es6模板字符串
+            url:`${config.apiBaseUrl}theme/by/names`,
+            //url:"http://se.7yue.pro/v1/theme/by/names",
+            method:"GET",
+            data:{
+                names:"t-1"
+            },
+            header:{
+                appKey:config.appKey
+            },
+            //使用箭头函数，解决this指向错误问题，可以保证this的指代是不变的
+            success:res => {
+                console.log(res);
+                this.setData({
+                    topTheme:res.data[0]
+                })
+            }
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+        })
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  }
+    }
 })
